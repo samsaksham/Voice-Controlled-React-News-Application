fecb5bc7a22efa3d8b78b26d504c6029e59189ea
samsaksham <rajput.saksham29@gmail.com>
2021-12-28 18:50:22 +0530

    Second commit

---
+++ build/precache-manifest.f7a4a15fc6f4788fc94619a875bba6d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8dec944ec16f3d93bfeaca6fd6b451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e43d22daed18d6015056",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "4a24e5c86d4557b32542",
+    "url": "/static/js/2.61eaf46a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.61eaf46a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e43d22daed18d6015056",
+    "url": "/static/js/main.4201e33b.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/static/media/logo.b0a1eb7c.png"
+  }
+]);
